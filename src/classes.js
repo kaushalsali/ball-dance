4d44260130bdbfa9045847e512f70951eb6e2156
Kaushal Sali <salikaushal@gmail.com>
2019-12-05 11:35:50 +0100

    Ball trails implemented. Need to set trail alpha better.

--- src/classes.js
+++ src/classes.js
@@ -1,40 +1,84 @@
 
 class Ball {
 
-    constructor(startX, startY, radius) {
+    constructor(startX, startY, radius, color) {
 	this.x = startX;
 	this.y = startY;
-	this.xStep = random(-5,5);
-	this.yStep = random(-5,5);
 	this.radius = radius;
-
-	let options = {
+	
+	this.trailHistory = [];
+	this.trailLength = 100;
+	this.minTrailRadius = radius / 3;
+	
+	this.body = Matter.Bodies.circle(startX, startY, radius, {
 	    friction: 0,
 	    frictionAir: 0,
 	    frictionStatic: 0,
 	    restitution: 1,
 	    // mass: random(1,2)
-	}
-	this.body = Matter.Bodies.circle(startX, startY, radius, options)		
+	});
+	
+	this.trailHistory.push(this.getPosition());
+
+	this.color = color;
+	this.alpha = 255;
+
     }
 
     getBody() {
 	return this.body;
     }
+
+    getPosition() {
+	return this.body.position;
+    }
+
+    getAngle() {
+	return this.body.angle;
+    }
+
+    calcTrailRadius(i) {	
+	let radiusIncFactor = (this.radius - this.minTrailRadius) / this.trailLength;
+	return this.minTrailRadius + (i * radiusIncFactor);
+    }
+
+    calcTrailAlpha(i) {
+	let alphaIncFactor = this.alpha / (this.trailLength - 1);
+	return Math.pow(i * alphaIncFactor, 0.5);
+    }
+
+    update() {
+	let pos = this.getPosition()
+	// let angle = this.getAngle();
+	this.trailHistory.push({x: pos.x, y: pos.y});
+
+	// this.trailHistory = this.trailHistory.slice(0, 10);
+	this.trailHistory = this.trailHistory.slice(this.trailHistory.length - this.trailLength, this.trailHistory.length);
+	
+    }
     
-    draw() {	
-	var pos = this.body.position;
-	var angle = this.body.angle;
-	push();
-	translate(pos.x, pos.y);
-	rotate(angle);
-	strokeWeight(2);
-	stroke(0);
-	fill(127);
-	ellipseMode(RADIUS);
-	ellipse(0, 0, this.radius);
-	// ellipse(0, 0, random(this.radius-0.5, this.radius+0.5), random(this.radius-0.5, this.radius+0.5));
-	pop();
+    draw() {
+	let alphaIncFac = this.alpha / this.trailLength;
+	console.log(this.trailHistory.length)
+	for (let i=0; i<this.trailHistory.length; i++) {
+	    if (i== this.trailLength-1)
+		console.log(calcTrailAlpha(i))
+	    push();
+	    translate(this.trailHistory[i].x, this.trailHistory[i].y);
+	    // rotate(angle);
+	    strokeWeight(0);
+	    stroke(0);
+	    fill(this.color, this.calcTrailAlpha(i));
+
+	    ellipseMode(RADIUS);
+	    ellipse(0, 0,  this.calcTrailRadius(i));
+	    // ellipse(0, 0, random(this.radius-0.5, this.radius+0.5), random(this.radius-0.5, this.radius+0.5)
+
+	    pop();
+	    // console.log(this.trailHistory[i]);
+	}
+	// console.log("---");
+
     }     
 }
 
